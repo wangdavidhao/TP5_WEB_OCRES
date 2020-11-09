c886f8ed17076f80f70cf16ed1d90067d7980da9
wangdavidhao <wangdavid1999@gmail.com>
2020-11-09 23:26:56 +0100

    Axios addes

--- tp5/routes/movie.js
+++ tp5/routes/movie.js
@@ -1,38 +1,54 @@
 var express = require('express');
 var router = express.Router();
 
+const axios = require('axios');
+
+const API_KEY = 'f907bc08';
+const API_URL = 'http://www.omdbapi.com/?apikey=';
+
+
+//Array d'objet movies => mini bdd
 movies = [
     {
         id:0,
-        name:"Avengers"
+        name:"Avengers",
+        annee:2012
     },
     {
         id:1,
-        name:"Star Wars"
+        name:"Star Wars",
+        annee:2008
     }
 ]
 
+
+//GET
 router.get('/', (req, res) => {
-    res.status(200).json({movies});
+    //res.status(200).json({movies});
+    axios.get(API_URL+API_KEY+'&s=avengers')
+       .then(response => res.json(response.data))
+       .catch(err => res.secn(err));
 });
 
+//POST
 router.post('/', (req, res) => {
 
     const {name} = req.body;
     const id = 2;
     movies.push({id,name});
-    res.json({
+    res.status(200).json({
         msg:"Succesfully added !",
         movies:movies
     });
 
 });
 
+//PUT
 router.put('/:id', (req, res) => {
     const {id} = req.params;
     const {name}= req.body;
-    const movieToUpdate = movies.find(movie => movie.id === parseInt(id));
-    movieToUpdate.name = name;
+    const movieToUpdate = movies.find(movie => movie.id === parseInt(id)); //string to int
+    movieToUpdate.name = name; //Update the name
 
     res.status(200).json({
         msg:`Update the ${id} user`,
@@ -40,6 +56,7 @@ router.put('/:id', (req, res) => {
     })
 });
 
+//DELETE
 router.delete('/:id', (req, res) => {
 
     const {id} = req.params;
